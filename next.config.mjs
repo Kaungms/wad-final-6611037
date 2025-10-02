73ac8e6860a1c008cca8630a2f0c581a3e1f4146
Kaungms <u6611037@au.edu>
2025-10-02 09:51:29 +0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
+  basePath: '/fin-customer',
+  assetPrefix: '/fin-customer',
 };
 
 export default nextConfig;
